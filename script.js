1666a1117d0019ee80cfb24c059be3191ed4e0ee
Keshav Sinha <keshavsinha2008@gmail.com>
2024-11-06 22:18:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,529 @@
+// Configuration for chapters and pages
+const chapters = {
+  
+  
+	1: [
+            'images/0/0001.jpg',
+            'images/0/0002.jpg',
+            'images/0/0003.jpg',
+            'images/0/0004.jpg',
+            'images/0/0005.jpg',
+            'images/0/0006.jpg',
+            'images/0/0007.jpg',
+            'images/0/0008.jpg',
+            'images/0/0009.jpg',
+            'images/0/0010.jpg',
+            'images/0/0011.jpg',
+            'images/0/0012.jpg',
+            'images/0/0013.jpg',
+            'images/0/0014.jpg',
+            'images/0/0015.jpg',
+            'images/0/0016.jpg',
+            'images/0/0017.jpg',
+        ],
+		
+        2: [
+           'images/1/0001.jpg',
+		   'images/1/0002.jpg',
+		   'images/1/0003.jpg',
+		   'images/1/0004.jpg',
+		   
+        ],
+        
+		 3: [
+           'images/2/0001.jpg',
+		   'images/2/0002.jpg',
+		   'images/2/0003.jpg',
+		   'images/2/0004.jpg',
+		   'images/2/0005.jpg',
+            'images/2/0006.jpg',
+            'images/2/0007.jpg',
+            'images/2/0008.jpg',
+            'images/2/0009.jpg',
+            'images/2/0010.jpg',
+            'images/2/0011.jpg',
+            'images/2/0012.jpg',
+        ],
+		
+		4: [
+           'images/3/0001.jpg',
+		   'images/3/0002.jpg',
+		   'images/3/0003.jpg',
+		   
+        ],
+		
+			5: [
+              'images/4/0001.jpg',
+		   'images/4/0002.jpg',
+		   'images/4/0003.jpg',
+		   'images/4/0004.jpg',
+		   'images/4/0005.jpg',
+            'images/4/0006.jpg',
+            'images/4/0007.jpg',
+            'images/4/0008.jpg',
+		   
+        ],
+		
+		
+		6: [
+            'images/5/0001.jpg',
+            'images/5/0002.jpg',
+            'images/5/0003.jpg',
+            'images/5/0004.jpg',
+            'images/5/0005.jpg',
+            'images/5/0006.jpg',
+            'images/5/0007.jpg',
+            'images/5/0008.jpg',
+            'images/5/0009.jpg',
+            'images/5/0010.jpg',
+            'images/5/0011.jpg',
+            'images/5/0012.jpg',
+            'images/5/0013.jpg',
+            'images/5/0014.jpg',
+            'images/5/0015.jpg',
+            'images/5/0016.jpg',
+            'images/5/0017.jpg',
+			'images/5/0018.jpg',
+			'images/5/0019.jpg',
+			'images/5/0020.jpg',
+			'images/5/0021.jpg',
+			'images/5/0022.jpg',
+        ],
+		
+		
+		
+		7: [
+            'images/6/0001.jpg',
+            'images/6/0002.jpg',
+            'images/6/0003.jpg',
+            'images/6/0004.jpg',
+            'images/6/0005.jpg',
+            'images/6/0006.jpg',
+            'images/6/0007.jpg',
+            'images/6/0008.jpg',
+            'images/6/0009.jpg',
+            'images/6/0010.jpg',
+            'images/6/0011.jpg',
+            'images/6/0012.jpg',
+            'images/6/0013.jpg',
+            'images/6/0014.jpg',
+        ],
+		
+		8: [
+            'images/7/0001.jpg',
+            'images/7/0002.jpg',
+            'images/7/0003.jpg',
+            'images/7/0004.jpg',
+            'images/7/0005.jpg',
+            'images/7/0006.jpg',
+            'images/7/0007.jpg',
+            'images/7/0008.jpg',
+            'images/7/0009.jpg',
+            'images/7/0010.jpg',
+            'images/7/0011.jpg',
+            'images/7/0012.jpg',
+            'images/7/0013.jpg',
+            
+        ],
+		
+		9: [
+            'images/8/0001.jpg',
+            'images/8/0002.jpg',
+            'images/8/0003.jpg',
+            'images/8/0004.jpg',
+            'images/8/0005.jpg',
+            'images/8/0006.jpg',
+            'images/8/0007.jpg',
+            'images/8/0008.jpg',
+            'images/8/0009.jpg',
+            'images/8/0010.jpg',
+            'images/8/0011.jpg',
+            'images/8/0012.jpg',
+            'images/8/0013.jpg',
+            'images/8/0014.jpg',
+            'images/8/0015.jpg',
+            'images/8/0016.jpg',
+            'images/8/0017.jpg',
+			'images/8/0018.jpg',
+			'images/8/0019.jpg',
+			'images/8/0020.jpg',
+        ],
+		
+		
+		10: [
+            'images/9/0001.jpg',
+            'images/9/0002.jpg',
+            'images/9/0003.jpg',
+            'images/9/0004.jpg',
+            'images/9/0005.jpg',
+            'images/9/0006.jpg',
+            'images/9/0007.jpg',
+            'images/9/0008.jpg',
+            'images/9/0009.jpg',
+            'images/9/0010.jpg',
+            'images/9/0011.jpg',
+            'images/9/0012.jpg',
+            'images/9/0013.jpg',
+            'images/9/0014.jpg',
+            'images/9/0015.jpg',
+            'images/9/0016.jpg',
+			'images/9/0017.jpg',
+			'images/9/0018.jpg',
+			'images/9/0019.jpg',
+			'images/9/0020.jpg',
+			'images/9/0021.jpg',
+			'images/9/0022.jpg',
+			'images/9/0023.jpg',
+			'images/9/0024.jpg',
+			'images/9/0025.jpg',
+            'images/9/0026.jpg',
+			'images/9/0027.jpg',
+			
+        ],
+		
+		11: [
+            'images/10/0001.jpg',
+            'images/10/0002.jpg',
+            'images/10/0003.jpg',
+            'images/10/0004.jpg',
+            'images/10/0005.jpg',
+            'images/10/0006.jpg',
+            'images/10/0007.jpg',
+            'images/10/0008.jpg',
+            'images/10/0009.jpg',
+            'images/10/0010.jpg',
+            'images/10/0011.jpg',
+            'images/10/0012.jpg',
+            'images/10/0013.jpg',
+            'images/10/0014.jpg',
+            'images/10/0015.jpg',
+            'images/10/0016.jpg',
+            'images/10/0017.jpg',
+			'images/10/0018.jpg',
+        ],
+		
+		12: [
+            'images/11/0001.jpg',
+            'images/11/0002.jpg',
+            'images/11/0003.jpg',
+            'images/11/0004.jpg',       
+			
+        ],
+		
+		13: [
+            'images/12/0001.jpg',
+            'images/12/0002.jpg',
+            'images/12/0003.jpg',
+            'images/12/0004.jpg',
+            'images/12/0005.jpg',
+
+        ],
+		
+		
+		14: [
+            'images/13/0001.jpg',
+            'images/13/0002.jpg',
+            'images/13/0003.jpg',
+            'images/13/0004.jpg',
+            'images/13/0005.jpg',
+            'images/13/0006.jpg',
+            'images/13/0007.jpg',
+            'images/13/0008.jpg',
+            'images/13/0009.jpg',
+            'images/13/0010.jpg',
+            'images/13/0011.jpg',
+            'images/13/0012.jpg',
+            'images/13/0013.jpg',
+			
+        ],
+		
+		15: [
+            'images/14/0001.jpg',
+            'images/14/0002.jpg',
+            'images/14/0003.jpg',
+            'images/14/0004.jpg',
+            'images/14/0005.jpg',
+            'images/14/0006.jpg',
+            'images/14/0007.jpg',
+            'images/14/0008.jpg',
+            'images/14/0009.jpg',
+            'images/14/0010.jpg',
+            'images/14/0011.jpg',
+            'images/14/0012.jpg',
+            'images/14/0013.jpg',
+            'images/14/0014.jpg',
+            'images/14/0015.jpg',
+            'images/14/0016.jpg',
+            'images/14/0017.jpg',
+			'images/14/0018.jpg',
+			'images/14/0019.jpg',
+			'images/14/0020.jpg',
+			'images/14/0021.jpg',
+			'images/14/0022.jpg',
+			'images/14/0023.jpg',
+			'images/14/0024.jpg',
+			
+        ],
+		
+		16: [
+            'images/15/0001.jpg',
+            'images/15/0002.jpg',
+            'images/15/0003.jpg',
+            'images/15/0004.jpg',
+            'images/15/0005.jpg',
+			
+        ],
+		
+		17: [
+            'images/16/0001.jpg',
+            'images/16/0002.jpg',
+            'images/16/0003.jpg',
+            'images/16/0004.jpg',
+            'images/16/0005.jpg',
+            'images/16/0006.jpg',
+            'images/16/0007.jpg',
+            'images/16/0008.jpg',
+            'images/16/0009.jpg',
+            'images/16/0010.jpg',
+            'images/16/0011.jpg',
+            'images/16/0012.jpg',
+            'images/16/0013.jpg',
+            'images/16/0014.jpg',
+            'images/16/0015.jpg',
+            'images/16/0016.jpg',
+            'images/16/0017.jpg',
+			'images/16/0018.jpg',
+			'images/16/0019.jpg',
+        ],
+		
+		18: [
+            'images/17/0001.jpg',
+            'images/17/0002.jpg',
+            'images/17/0003.jpg',
+            'images/17/0004.jpg',
+            'images/17/0005.jpg',
+            'images/17/0006.jpg',
+            'images/17/0007.jpg',
+            'images/17/0008.jpg',
+            'images/17/0009.jpg',
+            'images/17/0010.jpg',
+            'images/17/0011.jpg',
+            'images/17/0012.jpg',
+            'images/17/0013.jpg',
+            'images/17/0014.jpg',
+            'images/17/0015.jpg',
+            'images/17/0016.jpg',
+            'images/17/0017.jpg',
+			'images/17/0018.jpg',
+			'images/17/0019.jpg',
+			'images/17/0020.jpg',
+			
+        ],
+		
+		19: [
+            'images/18/0001.jpg',
+			'images/18/0002.jpg',
+			'images/18/0003.jpg',
+			'images/18/0004.jpg',
+			'images/18/0005.jpg',
+			'images/18/0006.jpg',
+			'images/18/0007.jpg',
+			'images/18/0008.jpg',
+			'images/18/0009.jpg',
+			'images/18/0010.jpg',
+			'images/18/0011.jpg',
+			'images/18/0012.jpg',
+			'images/18/0013.jpg',
+			'images/18/0014.jpg',
+			'images/18/0015.jpg',
+			'images/18/0016.jpg',
+			'images/18/0017.jpg',
+			'images/18/0018.jpg',
+			'images/18/0019.jpg',
+			'images/18/0020.jpg',
+			'images/18/0021.jpg',
+			'images/18/0022.jpg',
+			'images/18/0023.jpg',
+			'images/18/0024.jpg',
+			'images/18/0025.jpg',
+			'images/18/0026.jpg',
+			'images/18/0027.jpg',
+			'images/18/0028.jpg',
+			'images/18/0029.jpg',
+			'images/18/0030.jpg',
+			'images/18/0031.jpg',
+			'images/18/0032.jpg',
+			'images/18/0033.jpg',
+			'images/18/0034.jpg',
+			'images/18/0035.jpg',
+			'images/18/0036.jpg',
+			'images/18/0037.jpg',
+			'images/18/0038.jpg',
+            'images/18/0039.jpg',
+            'images/18/0040.jpg',
+            
+			],
+		
+		20: [
+            'images/19/0001.jpg',
+            'images/19/0002.jpg',
+            'images/19/0003.jpg',
+			
+        ],
+		
+		21: [
+            'images/20/0001.jpg',
+            'images/20/0002.jpg',
+            'images/20/0003.jpg',
+            'images/20/0004.jpg',
+            'images/20/0005.jpg',
+            'images/20/0006.jpg',
+            'images/20/0007.jpg',
+            'images/20/0008.jpg',
+            'images/20/0009.jpg',
+            'images/20/0010.jpg',
+            'images/20/0011.jpg',
+            'images/20/0012.jpg',
+            'images/20/0013.jpg',
+            'images/20/0014.jpg',
+            'images/20/0015.jpg',
+            'images/20/0016.jpg',
+            'images/20/0017.jpg',
+			'images/20/0018.jpg',
+			'images/20/0019.jpg',
+			'images/20/0020.jpg',
+			'images/20/0021.jpg',
+			'images/20/0022.jpg',
+			'images/20/0023.jpg',
+			'images/20/0024.jpg',
+			'images/20/0025.jpg',
+		
+			
+        ],
+		
+		22: [
+            'images/21/0001.jpg',
+            'images/21/0002.jpg',
+            'images/21/0003.jpg',
+            'images/21/0004.jpg',
+            'images/21/0005.jpg',
+            'images/21/0006.jpg',
+            'images/21/0007.jpg',
+            'images/21/0008.jpg',
+            'images/21/0009.jpg',
+            'images/21/0010.jpg',
+            'images/21/0011.jpg',
+            'images/21/0012.jpg',
+            'images/21/0013.jpg',
+            'images/21/0014.jpg',
+            'images/21/0015.jpg',
+            'images/21/0016.jpg',
+            'images/21/0017.jpg',
+			'images/21/0018.jpg',
+			'images/21/0019.jpg',
+			'images/21/0020.jpg',
+			'images/21/0021.jpg',
+			'images/21/0022.jpg',
+			'images/21/0023.jpg',
+			'images/21/0024.jpg',
+			'images/21/0025.jpg',
+			'images/21/0026.jpg',
+			'images/21/0027.jpg',
+			'images/21/0028.jpg',
+			'images/21/0029.jpg',
+			'images/21/0030.jpg',
+			'images/21/0031.jpg',
+			'images/21/0032.jpg',
+			'images/21/0033.jpg',
+			
+        ],
+		
+		23: [
+            'images/22/0001.jpg',
+            'images/22/0002.jpg',
+            'images/22/0003.jpg',
+            'images/22/0004.jpg',
+            'images/22/0005.jpg',
+            'images/22/0006.jpg',
+            'images/22/0007.jpg',
+            'images/22/0008.jpg',
+            'images/22/0009.jpg',
+            'images/22/0010.jpg',
+            'images/22/0011.jpg',
+            'images/22/0012.jpg',
+            'images/22/0013.jpg',
+            'images/22/0014.jpg',
+            'images/22/0015.jpg',
+            'images/22/0016.jpg',
+            'images/22/0017.jpg',
+			'images/22/0018.jpg',
+			'images/22/0019.jpg',
+			'images/22/0020.jpg',
+			'images/22/0021.jpg',
+			'images/22/0022.jpg',
+			'images/22/0023.jpg',
+        ]
+	
+};
+
+let currentChapter = null;
+let currentPage = 0;
+
+// Load chapters in dropdown menu
+function loadChapters() {
+    const chapterList = document.getElementById("chapterList");
+    for (const [chapter, pages] of Object.entries(chapters)) {
+        const a = document.createElement("a");
+        a.className = "dropdown-item text-light";
+        a.textContent = `Chapter ${chapter}`;
+        a.href = "#";
+        a.onclick = () => selectChapter(parseInt(chapter));
+        chapterList.appendChild(a);
+    }
+}
+
+// Select a chapter
+function selectChapter(chapter) {
+    currentChapter = chapter;
+    currentPage = 0;
+    document.getElementById("chapterTitle").textContent = `Chapter ${chapter}`;
+    updatePageDisplay();
+}
+
+// Update page display with the current image
+function updatePageDisplay() {
+    const pageDisplay = document.getElementById("pageDisplay");
+    const pages = chapters[currentChapter];
+    
+    if (pages && pages.length > 0) {
+        pageDisplay.innerHTML = `<img src="${pages[currentPage]}" alt="Page Image">`;
+        document.getElementById("prevButton").disabled = currentPage === 0;
+        document.getElementById("nextButton").disabled = currentPage === pages.length - 1;
+    }
+}
+
+// Show previous page
+function showPreviousPage() {
+    if (currentPage > 0) {
+        currentPage--;
+        updatePageDisplay();
+    }
+}
+
+// Show next page
+function showNextPage() {
+    const pages = chapters[currentChapter];
+    if (pages && currentPage < pages.length - 1) {
+        currentPage++;
+        updatePageDisplay();
+    }
+}
+
+// Toggle light and dark mode
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("light-mode");
+}
+
+// Initialize the app
+loadChapters();
